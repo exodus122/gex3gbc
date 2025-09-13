8c75e7c8ae1a317d5465ad90dbb0fe42d75b09f3
Exodus <7614885+exodus122@users.noreply.github.com>
2025-09-13 19:21:58 -0400

    Extract images from bank 1

--- tools/extract_images.py
+++ tools/extract_images.py
@@ -79,7 +79,7 @@ def extract_bank_1f():
                 chunk_counter = chunk_counter + 1
         i = i + 1
 
-def extract_gex3_sprites_vertical():
+def extract_object_sprites():
     banks = ["008", "009", "00a", "00a", "00b", "00c", "00d", "00e", "00f", "010", "01d", "01e"]
     banks2 = ["08", "09", "0a", "0a", "0b", "0c", "0d", "0e", "0f", "10", "1d", "1e"]
     addrs = [0x4000, 0x4000, 0x4000, 0x6000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000]
@@ -190,40 +190,54 @@ def extract_tilesets():
         os.system('rgbgfx --reverse 16 -o banks/bank_'+bank+'.bin banks/tilesets/tileset_'+bank+'.png')
 
 def extract_bank_03():
-    # extracts the two sets of sprites at the top of bank 03
+    # extracts the 3 images at the top of bank 03
     bank = "003"
     bank2 = "03"
     os.system('mkdir -p banks/bank_'+bank+'/')
 
     bgcollision_tileset_img_bin = open('./banks/bank_'+bank+'.bin', 'rb').read()[0x0100:0x0400]
-
     out = open('./banks/bank_'+bank+'/image_'+bank+'_4100.bin', "wb")
     out.write(bgcollision_tileset_img_bin)
     out.close()
-    
     os.system('rgbgfx --reverse 16 -d 1 -o banks/bank_'+bank+'/image_'+bank+'_4100.bin banks/bank_'+bank+'/image_'+bank+'_4100.png')
 
 
     pawprint_img_bin = open('./banks/bank_'+bank+'.bin', 'rb').read()[0x400:0x580]
-
     out = open('./banks/bank_'+bank+'/image_'+bank+'_4400.bin', "wb")
     out.write(pawprint_img_bin)
     out.close()
-    
     os.system('rgbgfx --reverse 12 --columns -o banks/bank_'+bank+'/image_'+bank+'_4400.bin banks/bank_'+bank+'/image_'+bank+'_4400.png')
 
     numbers_img_bin = open('./banks/bank_'+bank+'.bin', 'rb').read()[0x580:0x6E0]
-
     out = open('./banks/bank_'+bank+'/image_'+bank+'_4580.bin', "wb")
     out.write(numbers_img_bin)
     out.close()
-    
     os.system('rgbgfx --reverse 11 --columns -o banks/bank_'+bank+'/image_'+bank+'_4580.bin banks/bank_'+bank+'/image_'+bank+'_4580.png')
 
+def extract_bank_01():
+    # extracts the images at the bottom of bank 01
+    bank = "001"
+    bank2 = "01"
+    os.system('mkdir -p banks/bank_'+bank+'/')
+    starts = [0x5c79, 0x6069, 0x64e9, 0x66f9, 0x6f48]
+    ends = [0x6069, 0x64e9, 0x66f9, 0x6f39, 0x70a8]
+    widths = [63, 8, 33, 66, 11]
+
+    for i in range(0, len(starts)):
+        addr = starts[i]
+        addr_str = f"{addr:0{4}x}"
+        img_bin = open('./banks/bank_'+bank+'.bin', 'rb').read()[addr-0x4000:ends[i]-0x4000]
+        out = open('./banks/bank_'+bank+'/image_'+bank+'_'+addr_str+'.bin', "wb")
+        out.write(img_bin)
+        out.close()
+        os.system('rgbgfx --reverse '+str(widths[i])+' --columns -o banks/bank_'+bank+'/image_'+bank+'_'+addr_str+'.bin banks/bank_'+bank+'/image_'+bank+'_'+addr_str+'.png')
+
+
 #extract_banks()
-#extract_gex3_sprites_vertical()
+#extract_object_sprites()
 #extract_bank_1f()
 #extract_bank_06()
 #extract_bank_11()
 #extract_tilesets()
-extract_bank_03()
+#extract_bank_03()
+extract_bank_01()
